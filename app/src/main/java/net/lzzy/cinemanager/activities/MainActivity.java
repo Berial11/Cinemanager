1fafc9a9dbf49bf752a4b425c92f415aed8439ba
Berial11 <h943199571@yahoo.com>
2019-03-26 15:25:44 +0800

    2019年3月26日15:25:15

--- app/src/main/java/net/lzzy/cinemanager/activities/MainActivity.java
+++ app/src/main/java/net/lzzy/cinemanager/activities/MainActivity.java
@@ -3,17 +3,61 @@ package net.lzzy.cinemanager.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.SearchView;
+import android.widget.TextView;
 
 import net.lzzy.cinemanager.R;
 
 /**
  * @author Administrator
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private LinearLayout layoutMenu;
+    private TextView tvTitle;
+    private SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setTitleMenu();
+    }
+
+    private void setTitleMenu(){
+        layoutMenu=findViewById(R.id.bar_title_layout_menu);
+        layoutMenu.setVisibility(View.GONE);
+        findViewById(R.id.bar_title_layout_menu).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int visible=layoutMenu.getVisibility()==View.VISIBLE?View.GONE:View.VISIBLE;
+                layoutMenu.setVisibility(visible);
+
+            }
+        });
+        tvTitle=findViewById(R.id.bar_title_tv);
+        tvTitle.setText(R.string.bar_title_tv_my_order);
+        searchView=findViewById(R.id.bar_title_sv);
+        findViewById(R.id.bar_title_iv_menu).setOnClickListener(this);
+        findViewById(R.id.bar_title_tv_my_order).setOnClickListener(this);
+        findViewById(R.id.bar_title_tv_add_order).setOnClickListener(this);
+        findViewById(R.id.bar_title_tv_view_cinema).setOnClickListener(this);
+        findViewById(R.id.bar_title_tv_add_cinema).setOnClickListener(this);
+        findViewById(R.id.bar_title_tv_exit).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.exit(0);
+            }
+        });
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
